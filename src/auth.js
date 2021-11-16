696ec5d6f19d94b9d2d80953915b18879eab1e16
Taylor Lindsey <justtaylorlindsey@gmail.com>
2021-11-16 11:59:31 -0500

    Converted auth into discrete module.

--- src/auth.js
+++ src/auth.js
@@ -1,55 +1,79 @@
 const Hapi = require("@hapi/hapi");
 const axios = require("axios");
-const { client_id, client_secret } = require("./client.json");
+const { client_id, client_secret, scopes } = require("./client.json");
 
+// sets base64Client
+const base64Client = new Buffer.from(`${client_id}:${client_secret}`).toString(
+  "base64"
+);
+
+// TODO: convert to TypeScript
 const server = Hapi.server({
   port: 8888,
   host: "localhost",
 });
 
-const init = async () => {
-  await server.start();
-  console.log("Server running on %s", server.info.uri);
-  console.log();
-  console.log(`Click to authorize app: ${urlBuilder()}`);
-};
+const start = () => {
+  return new Promise(async (resolve) => {
+    server.route({
+      method: "GET",
+      path: "/callback",
+      handler: async ({ query: { code } }) => {
+        resolve(code);
+        return "Success";
+      },
+    });
 
-server.route({
-  method: "GET",
-  path: "/callback",
-  handler: async ({ query: { code } }) => {
-    setToken(code);
-    return "Success";
-  },
-});
+    await server.start();
+    console.log("Server running on %s\n", server.info.uri);
+
+    url = "https://accounts.spotify.com/authorize?response_type=code";
+    url += `&client_id=${client_id}`;
+    url += `&scope=${encodeURIComponent(scopes.join(" "))}`;
+    url += `&redirect_uri=${encodeURIComponent(
+      "http://localhost:8888/callback"
+    )}`;
+    console.log(`Click to authorize app: ${url}`);
+  });
+};
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
 
-init();
+const setToken = () => {
+  return start()
+    .then((code) => {
+      return axios.post(
+        "https://accounts.spotify.com/api/token",
+        new URLSearchParams({
+          grant_type: "authorization_code",
+          code: code,
+          redirect_uri: "http://localhost:8888/callback",
+        }),
+        {
+          headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${base64Client}`,
+          },
+          params: {},
+        }
+      );
+    })
+    .then(({ data }) => {
+      server.stop();
+      return data;
+    });
+};
 
-function urlBuilder() {
-  let scopes = "user-read-private user-read-email";
-  let url = "";
-  scopes = "&scope=" + encodeURIComponent(scopes);
-  url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}${scopes}&redirect_uri=${encodeURIComponent(
-    "http://localhost:8888/callback"
-  )}`;
-  return url;
-}
-
-const setToken = (code) => {
-  let buff = new Buffer.from(`${client_id}:${client_secret}`);
-  let base64Client = buff.toString("base64");
+const refreshToken = (refresh_token) => {
   return axios
     .post(
       "https://accounts.spotify.com/api/token",
       new URLSearchParams({
-        code: code,
-        redirect_uri: "http://localhost:8888/callback",
-        grant_type: "authorization_code",
+        grant_type: "refresh_token",
+        refresh_token,
       }),
       {
         headers: {
@@ -60,12 +84,14 @@ const setToken = (code) => {
       }
     )
     .then(({ data }) => {
-      console.log(data);
-      // TODO: write token
-      // data.access_token
-      // data.refresh_token
+      return data;
     })
     .catch((err) => {
       console.log(err);
     });
+};
+
+module.exports = {
+  setToken: setToken,
+  refreshToken: refreshToken,
 };
