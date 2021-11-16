c1af6a49e76d095d6bf77252edaf0e037e15aecc
Taylor Lindsey <justtaylorlindsey@gmail.com>
2021-11-16 11:59:31 -0500

    Moved spotify functions to discrete module.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 #!/usr/bin/env node
 const fs = require("fs");
-const axios = require("axios");
 const { program } = require("commander");
-const { DateTime } = require("luxon");
-const auth = require("./auth");
-
-const spotify = axios.create({ baseURL: "https://api.spotify.com/v1/" });
-let { access_token, refresh_token } = require("./token.json");
+const spotify = require("./spotify");
 
 program
   .option(
@@ -19,83 +14,14 @@ program
 // TODO: misc tags
 // TODO: explicit
 // TODO: release date
+
 program.parse(process.argv);
 const options = program.opts();
 
 const start = async () => {
-  if (access_token == undefined) {
-    let data = await auth.setToken();
-    // TODO: convert token updating to a function?
-    access_token = data.access_token;
-    fs.writeFile(
-      `${__dirname}/token.json`,
-      JSON.stringify(data, null, 2),
-      (err) => {
-        if (err) {
-          throw err;
-        }
-        console.log("token.json updated!");
-      }
-    );
-  }
-  let { data: current } = await spotify
-    .get("me/player/currently-playing", {
-      params: {
-        market: "from_token",
-      },
-      headers: { Authorization: `Bearer ${access_token}` },
-    })
-    .catch(async () => {
-      console.log("Access token expired, trying to refresh.");
-      let data = await auth.refreshToken(refresh_token);
-      access_token = data.access_token;
-      data = {
-        refresh_token,
-        ...data,
-      };
-      fs.writeFile(
-        `${__dirname}/token.json`,
-        JSON.stringify(data, null, 2),
-        (err) => {
-          if (err) {
-            throw err;
-          }
-          console.log("token.json updated!");
-        }
-      );
-      return spotify.get("me/player/currently-playing", {
-        params: {
-          market: "from_token",
-        },
-        headers: { Authorization: `Bearer ${access_token}` },
-      });
-    })
-    .catch(async () => {
-      console.log("Refresh failed, requesting new token.");
-      let data = await auth.setToken(refresh_token);
-      access_token = data.access_token;
-      fs.writeFile(
-        `${__dirname}/token.json`,
-        JSON.stringify(data, null, 2),
-        (err) => {
-          if (err) {
-            throw err;
-          }
-          console.log("token.json updated!");
-        }
-      );
-      return spotify.get("me/player/currently-playing", {
-        params: {
-          market: "from_token",
-        },
-        headers: { Authorization: `Bearer ${access_token}` },
-      });
-    });
+  let { data: current } = await spotify.getCurrentPlayback();
 
-  // TODO: move to a function
-  // build a string with the artists
-  let artistString = artistsToString(current.item.artists);
-
+  let artistString = spotify.artistsToString(current.item.artists);
   console.log(
     `Currently listening to '${current.item.name}' by ${artistString}.`
   );
@@ -106,199 +32,17 @@ const start = async () => {
 
   // fetch all playlists for caching and tagging
   if (options.refreshPlaylists) {
-    console.log("Downloading playlists with tag information...");
-    let { taggedPlaylists, tagMap } = await getPlaylistTags(access_token);
-    console.log(tagMap);
-    fs.writeFile(
-      `${__dirname}/playlists.json`,
-      JSON.stringify(taggedPlaylists, null, 2),
-      (err) => {
-        if (err) {
-          throw err;
-        }
-        console.log("playlists.json updated!");
-      }
-    );
+    await spotify.refreshPlaylistTags();
   }
 
   if (options.sort) {
-    let tags = sort(current.item);
+    let tags = await spotify.sort(current.item);
     console.log(tags);
   }
 
-  // let playlistID = "0qLcANYAyF4sMipVPx5pCc";
-
   // addToPlaylist(access_token, playlistID, current.item.uri);
-  // remove song from Liked Songs
-  // spotify
-  //   .delete("me/tracks", {
-  //     params: {
-  //       ids: current.item.id,
-  //     },
-  //     headers: { Authorization: `Bearer ${access_token}` },
-  //   })
-  //   .then(() => {
-  //     console.log(
-  //       `Track '${current.item.name}' by ${artistString} has been removed from Liked Songs.`
-  //     );
-  //   });
+  //
   // TODO: confirm or manually add and remove playlists.
 };
 
 start();
-
-const getPlaylists = (token, offset = 0) => {
-  return spotify.get("me/playlists", {
-    params: { limit: 50, offset },
-    headers: { Authorization: `Bearer ${token}` },
-  });
-};
-
-const getAllPlaylists = async (token) => {
-  let offset = 0;
-  let data = (await getPlaylists(token, offset)).data;
-  let allPlaylists = data.items;
-  while (data.items.length > 0) {
-    offset += 50;
-    data = (await getPlaylists(token, offset)).data;
-    allPlaylists = allPlaylists.concat(data.items);
-  }
-  return allPlaylists;
-};
-
-const getPlaylistTags = async (token) => {
-  let tagMap = new Map();
-  let taggedPlaylists = [];
-  let playlists = await getAllPlaylists(token);
-  playlists.forEach((playlist) => {
-    if (playlist.description != undefined && playlist.description != "") {
-      let tags = playlist.description.split("#");
-      let mandatoryTags = [];
-      let optionalTags = [];
-      let excludeTags = [];
-      tags.shift();
-
-      tags.forEach((tag) => {
-        let name = playlist.name;
-        tag = tag.trim();
-        if (tag[0] == "?") {
-          tag = tag.substring(1);
-          optionalTags.push(tag);
-          name = `?${name}`;
-        } else if (tag[0] == "!") {
-          tag = tag.substring(1);
-          excludeTags.push(tag);
-          name = `!${name}`;
-        } else {
-          mandatoryTags.push(tag);
-        }
-
-        if (tagMap.has(tag)) {
-          tagMap.set(tag, tagMap.get(tag).concat([name]));
-        } else {
-          tagMap.set(tag, [name]);
-        }
-      });
-      if (
-        (mandatoryTags.length !== 0) |
-        (optionalTags.length !== 0) |
-        (excludeTags.length != 0)
-      ) {
-        taggedPlaylists.push({
-          name: playlist.name,
-          uri: playlist.uri,
-          mandatoryTags,
-          optionalTags,
-          excludeTags,
-        });
-      }
-    }
-  });
-  return {
-    taggedPlaylists: taggedPlaylists,
-    tagMap: tagMap,
-  };
-};
-
-// TODO: in ts make this work for arrays
-const addToPlaylist = async (token, playlist, song) => {
-  if (!(await playlistIncludes(token, playlist, song))) {
-    return spotify.post(
-      `playlists/${playlist}/tracks`,
-      {
-        uris: [song],
-      },
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
-  }
-};
-
-const playlistIncludes = (token, playlist, song) => {
-  return spotify
-    .get(`playlists/${playlist}/tracks`, {
-      param: { market: "from_token" },
-      headers: { Authorization: `Bearer ${token}` },
-    })
-    .then(({ data }) => {
-      let playlistIncludesSong = false;
-      data.items.forEach((item) => {
-        if (item.track.uri === song) {
-          playlistIncludesSong = true;
-        }
-      });
-      return playlistIncludesSong;
-    });
-};
-
-const artistsToString = (artists) => {
-  return artists.reduce((accumulator, artist, index, artists) => {
-    if (index === 0) {
-      return artist.name;
-    } else if (index === artists.length - 1) {
-      return `${accumulator}, and ${artist.name}`;
-    } else {
-      return `${accumulator}, ${artist.name}`;
-    }
-  }, "");
-};
-
-const sort = (song) => {
-  let tags = [];
-
-  if (!song.explicit) {
-    tags.push("clean");
-  }
-
-  if (song.popularity > 90) {
-    tags.push("trendy");
-  }
-
-  let release = DateTime.fromISO(song.album.release_date);
-  if (release < DateTime.fromISO("1940")) {
-    tags.push("oldies");
-  } else if (release < DateTime.fromISO("1950")) {
-    tags.push("40s");
-  } else if (release < DateTime.fromISO("1960")) {
-    tags.push("50s");
-  } else if (release < DateTime.fromISO("1970")) {
-    tags.push("60s");
-  } else if (release < DateTime.fromISO("1980")) {
-    tags.push("70s");
-  } else if (release < DateTime.fromISO("1990")) {
-    tags.push("80s");
-  } else if (release < DateTime.fromISO("2000")) {
-    tags.push("90s");
-  } else if (release < DateTime.fromISO("2010")) {
-    tags.push("00s");
-  } else if (release < DateTime.fromISO("2020")) {
-    tags.push("10s");
-  } else {
-    tags.push("20s");
-  }
-
-  // TODO: add manual tag questionaire
-
-  return tags;
-};
---
+++ src/spotify.js
@@ -0,0 +1,294 @@
+const axios = require("axios");
+const fs = require("fs");
+const { DateTime } = require("luxon");
+const auth = require("./auth");
+
+const spotify = axios.create({ baseURL: "https://api.spotify.com/v1/" });
+let { access_token, refresh_token } = require("./token.json");
+
+// TODO: convert this to typescript
+// TODO: add comprehensive JSDoc comments
+/**
+ * Checks the token on process startup.
+ */
+const checkToken = async () => {
+  if (access_token == undefined) {
+    await auth.setToken();
+    // TODO: convert token updating to a function?
+    access_token = data.access_token;
+    fs.writeFile(
+      `${__dirname}/token.json`,
+      JSON.stringify(data, null, 2),
+      (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log("token.json updated!");
+      }
+    );
+  }
+  getCurrentPlayback()
+    .catch(async () => {
+      // TODO: move this to an on unhandled rejection catchall
+      console.log("Access token expired, trying to refresh.");
+      updateToken(await auth.refreshToken(refresh_token));
+      return getCurrentPlayback();
+    })
+    .catch(async () => {
+      console.log("Refresh failed, requesting new token.");
+      updateToken(await auth.setToken(refresh_token));
+      return getCurrentPlayback();
+    });
+};
+
+/**
+ * Get information about the user’s current playback state, including track or episode, progress, and active device.
+ */
+const getCurrentPlayback = () => {
+  return spotify.get("me/player/currently-playing", {
+    params: {
+      market: "from_token",
+    },
+    headers: { Authorization: `Bearer ${access_token}` },
+  });
+};
+
+/**
+ * Updates the cached token data in token.json
+ * @param {*} data The access_token, refresh_token, and other token data.
+ */
+const updateToken = (data) => {
+  data = {
+    refresh_token,
+    ...data,
+  };
+  access_token = data.access_token;
+  fs.writeFile(
+    `${__dirname}/token.json`,
+    JSON.stringify(data, null, 2),
+    (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("token.json updated!");
+    }
+  );
+};
+
+const artistsToString = (artists) => {
+  return artists.reduce((accumulator, artist, index, artists) => {
+    if (index === 0) {
+      return artist.name;
+    } else if (index === artists.length - 1) {
+      return `${accumulator}, and ${artist.name}`;
+    } else {
+      return `${accumulator}, ${artist.name}`;
+    }
+  }, "");
+};
+
+/**
+ * Remove song from Liked Songs
+ * @param {*} song
+ */
+const unlikeSong = (song) => {
+  return spotify
+    .delete("me/tracks", {
+      params: {
+        ids: song.id,
+      },
+      headers: { Authorization: `Bearer ${access_token}` },
+    })
+    .then(() => {
+      console.log(
+        `Track '${current.item.name}' by ${artistString} has been removed from Liked Songs.`
+      );
+    });
+};
+
+/**
+ * Gets 50 of the current users playlists.
+ * @param {*} offset What index playlist to start fetching from.
+ * @returns
+ */
+const getPlaylists = (offset = 0) => {
+  return spotify.get("me/playlists", {
+    params: { limit: 50, offset },
+    headers: { Authorization: `Bearer ${access_token}` },
+  });
+};
+
+/**
+ * Gets all of the current user's playlists.
+ * @returns
+ */
+const getAllPlaylists = async () => {
+  let offset = 0;
+  let data = (await getPlaylists(offset)).data;
+  let allPlaylists = data.items;
+  while (data.items.length > 0) {
+    offset += 50;
+    data = (await getPlaylists(offset)).data;
+    allPlaylists = allPlaylists.concat(data.items);
+  }
+  return allPlaylists;
+};
+
+// TODO: in ts make this work for arrays
+const addToPlaylist = async (playlist, song) => {
+  if (!(await playlistIncludes(playlist, song))) {
+    return spotify.post(
+      `playlists/${playlist}/tracks`,
+      {
+        uris: [song],
+      },
+      {
+        headers: { Authorization: `Bearer ${access_token}` },
+      }
+    );
+  }
+};
+
+const playlistIncludes = (playlist, song) => {
+  return spotify
+    .get(`playlists/${playlist}/tracks`, {
+      param: { market: "from_token" },
+      headers: { Authorization: `Bearer ${access_token}` },
+    })
+    .then(({ data }) => {
+      let playlistIncludesSong = false;
+      data.items.forEach((item) => {
+        if (item.track.uri === song) {
+          playlistIncludesSong = true;
+        }
+      });
+      return playlistIncludesSong;
+    });
+};
+
+const getPlaylistTags = async () => {
+  let tagMap = new Map();
+  let taggedPlaylists = [];
+  let playlists = await getAllPlaylists();
+  playlists.forEach((playlist) => {
+    if (playlist.description != undefined && playlist.description != "") {
+      let tags = playlist.description.split("#");
+      let mandatoryTags = [];
+      let optionalTags = [];
+      let excludeTags = [];
+      tags.shift();
+
+      tags.forEach((tag) => {
+        let name = playlist.name;
+        tag = tag.trim();
+        if (tag[0] == "?") {
+          tag = tag.substring(1);
+          optionalTags.push(tag);
+          name = `?${name}`;
+        } else if (tag[0] == "!") {
+          tag = tag.substring(1);
+          excludeTags.push(tag);
+          name = `!${name}`;
+        } else {
+          mandatoryTags.push(tag);
+        }
+
+        if (tagMap.has(tag)) {
+          tagMap.set(tag, tagMap.get(tag).concat([name]));
+        } else {
+          tagMap.set(tag, [name]);
+        }
+      });
+      if (
+        (mandatoryTags.length !== 0) |
+        (optionalTags.length !== 0) |
+        (excludeTags.length != 0)
+      ) {
+        taggedPlaylists.push({
+          name: playlist.name,
+          uri: playlist.uri,
+          mandatoryTags,
+          optionalTags,
+          excludeTags,
+        });
+      }
+    }
+  });
+  return {
+    taggedPlaylists: taggedPlaylists,
+    tagMap: tagMap,
+  };
+};
+
+const refreshPlaylistTags = async () => {
+  console.log("Downloading playlists with tag information...");
+  let { taggedPlaylists, tagMap } = await getPlaylistTags(access_token);
+  console.log(tagMap);
+  fs.writeFile(
+    `${__dirname}/playlists.json`,
+    JSON.stringify(taggedPlaylists, null, 2),
+    (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("playlists.json updated!");
+    }
+  );
+};
+
+const sort = async (song) => {
+  let tags = [];
+
+  if (!song.explicit) {
+    tags.push("clean");
+  }
+
+  if (song.popularity > 90) {
+    tags.push("trendy");
+  }
+
+  let release = DateTime.fromISO(song.album.release_date);
+  if (release < DateTime.fromISO("1940")) {
+    tags.push("oldies");
+  } else if (release < DateTime.fromISO("1950")) {
+    tags.push("40s");
+  } else if (release < DateTime.fromISO("1960")) {
+    tags.push("50s");
+  } else if (release < DateTime.fromISO("1970")) {
+    tags.push("60s");
+  } else if (release < DateTime.fromISO("1980")) {
+    tags.push("70s");
+  } else if (release < DateTime.fromISO("1990")) {
+    tags.push("80s");
+  } else if (release < DateTime.fromISO("2000")) {
+    tags.push("90s");
+  } else if (release < DateTime.fromISO("2010")) {
+    tags.push("00s");
+  } else if (release < DateTime.fromISO("2020")) {
+    tags.push("10s");
+  } else {
+    tags.push("20s");
+  }
+
+  // TODO: add manual tag questionaire
+
+  return tags;
+};
+
+// TODO: see if this is a good flow
+checkToken();
+
+// TODO: hide some of these functions.
+module.exports = {
+  getCurrentPlayback,
+  updateToken,
+  artistsToString,
+  unlikeSong,
+  getPlaylists,
+  getAllPlaylists,
+  addToPlaylist,
+  playlistIncludes,
+  getPlaylistTags,
+  refreshPlaylistTags,
+  sort,
+};
